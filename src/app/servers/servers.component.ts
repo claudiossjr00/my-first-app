d5ef074de9f1eccb3be53c27ab09bfdbda64ba46
Claudio Souza <c.souza0804@gmail.com>
2019-06-22 18:46:15 -0300

    end of class number 37 - Adding ngStyle and ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = "No server was created!";
+  serverName : string = "";
+  serverCreated : boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  // Update server text without ngModel and two way bind
+  // onUpdateServerName (event: Event)
+  // {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
+
+  onCreateServer() 
+  {
+    this.serverCreated = true;
+    this.serverCreationStatus = `Server was created! Server name is ${this.serverName}`;
   }
 
 }
